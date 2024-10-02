77ad7618242ed19a04bc13d663c9a6020100260b
madotto.Axel@outlook.com <axelmadotto@MacBook-Air-de-Axel.local>
2024-10-02 10:55:40 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+require('dotenv').config()
+
+
+router.get("/movies" , (req,res)=>{
+    fetch (`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_MOVIE}`)
+    .then(response=>response.json())
+    .then(data => {
+        console.log(data.results.length)
+        res.json({movies : data.results})})
+    
+})
+
+module.exports = router;
